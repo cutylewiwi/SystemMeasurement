18e29b44d438f33cebda9374b5a1311f5c1abede
cuty-lewiwi <wuyudong5426@163.com>
2017-02-06 13:36:22 -0800

    cycles

---
+++ cycles.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+#include <unistd.h>
+
+#include "proj_timing.h"
+
+int main (int argc, const char * arg[]){
+    unsigned long long start;
+    unsigned long long end;
+    uint32_t low, low1;
+    uint32_t high, high1;
+    int i;
+    for (i = 0; i < ITERATIONS; i++) {
+        START_COUNT(low, high);
+        sleep(10);
+        STOP_COUNT(low1, high1);
+
+        start = ((unsigned long long) high << 32) | low;
+        end = ((unsigned long long) high1 << 32) | low1;
+
+        printf("%llu\t%llu\t%llu\n", start, end, end - start);
+    }
+
+    return 0;
+}
