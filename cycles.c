244d207df8076889b79838e4ba52116907118c4c
cuty-lewiwi <wuyudong5426@163.com>
2017-02-06 14:10:21 -0800

    polish cycles

--- cycles.c
+++ cycles.c
@@ -16,7 +16,9 @@ int main (int argc, const char * arg[]){
     unsigned long long end;
     uint32_t low, low1;
     uint32_t high, high1;
+    unsigned long long sum;
     int i;
+
     for (i = 0; i < ITERATIONS; i++) {
         START_COUNT(high, low);
         sleep(10);
@@ -32,9 +34,21 @@ int main (int argc, const char * arg[]){
         records[i] = end - start;
     }
 
+    sum = ;
     for (i = 0; i < ITERATIONS; i++) {
-        printf("%llu\n", records[i]);
+        printf("cycles sample %d: %llu\n", i, records[i]);
+
+        // As CPU frequency will be at the scale of 1GHZ -> 10 ^ 9 ~ 10 ^ 10
+        // unsigned long long will be save from overflow
+        sum += records[i];
     }
+
+    printf ("samples: %d\n", ITERATIONS);
+
+    // Under the scale of sum
+    // cast from unsigned longlong to double will be safe
+    printf ("mean: %lf\n", (double)sum / 10);
+    printf ("frequency: %.3lf\n", (double)sum / 1e10);
 
     return 0;
 }
