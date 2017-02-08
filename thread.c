96ef779e0d3c3df26ca43d044996f4aed162bcf0
cuty-lewiwi <wuyudong5426@163.com>
2017-02-08 11:53:02 -0800

    thread fix

--- thread.c
+++ thread.c
@@ -10,6 +10,9 @@
 #define ITERATIONS 10
 
 
+uint32_t lowt, hight;
+unsigned long long endthread;
+
 void *doSth(void *arg){
     STOP_COUNT(hight, lowt);
     endthread = end = ((unsigned long long) hight << 32) | lowt;
@@ -17,9 +20,9 @@ void *doSth(void *arg){
 
 int main () {
     unsigned long long start;
-    unsigned long long end, endthread;
-    uint32_t low, low1, lowt;
-    uint32_t high, high1, hight;
+    unsigned long long end;
+    uint32_t low, low1;
+    uint32_t high, high1;
     pthread_t td;
     int i;
 
@@ -28,7 +31,7 @@ int main () {
         START_COUNT(high, low);
         pthread_create(&td, NULL, doSth, NULL);
         STOP_COUNT(high1, low1);
-        
+
         pthread_join(td, NULL);
         start = ((unsigned long long) high << 32) | low;
         end = ((unsigned long long) high1 << 32) | low1;
