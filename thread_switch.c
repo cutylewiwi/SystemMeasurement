1da71c0a8ec98d107ab4f9b8e1cbe9e676cae2f5
cuty-lewiwi <wuyudong5426@163.com>
2017-02-08 14:27:38 -0800

    pthread switch fix

---
+++ thread_switch.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include "proj_timing.h"
+
+#define ITERATIONS 10
+
+
+uint32_t lowt, hight;
+unsigned long long endthread;
+int pipefd[2];
+char buf[1];
+
+void *thread_fn(void *arg){
+    // blocking the thread by read
+    read(pipefd[0], buf, 1);
+    STOP_COUNT(hight, lowt);
+    pthread_exit();
+}
+
+int main () {
+    unsigned long long start;
+    unsigned long long end;
+    uint32_t high, low;
+    pthread_t td;
+    int i;
+
+    WARMUP(high, low, high1, low1);
+    for (i = 0; i < ITERATIONS; i++) {
+        if (pipe(pipefd) == -1) {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+
+        pthread_create(&td, NULL, thread_fn, NULL);
+
+        sleep(1);
+        write(pipefd[1], buf, 1);
+        START_COUNT(high, low);
+        pthread_yield();
+
+        pthread_join(td, NULL);
+        start = ((unsigned long long) high << 32) | low;
+        endthread = ((unsigned long long) hight << 32) | lowt;
+
+
+        end = endthread < end ? endthread : end;
+
+        printf("%llu\n", end - start);
+    }
+
+    return 0;
+}
