831ff6e09400f05a2cc710527eff77f0b59b95ec
EvonX <xiangchnchn@gmail.com>
2017-02-08 02:51:24 -0800

    stat analysis

--- time_ovh.c
+++ time_ovh.c
@@ -5,17 +5,19 @@
 
 #include "proj_timing.h"
 
-#define ITERATIONS 10
+//#define ITERATIONS 10
 
-int main () {
+int main (int argc, char* argv[]) {
     unsigned long long start;
     unsigned long long end;
     uint32_t low, low1;
     uint32_t high, high1;
     int i;
 
+    int loop = atoi((const char *) argv[argc-1]);
+
     WARMUP(high, low, high1, low1);
-    for (i = 0; i < ITERATIONS; i++) {
+    for (i = 0; i < loop; i++) {
         START_COUNT(high, low);
         STOP_COUNT(high1, low1);
 
