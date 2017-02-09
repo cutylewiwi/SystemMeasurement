b33d591b0de8e96334aeb0fe7e29fdb76470e36a
EvonX <xiangchnchn@gmail.com>
2017-02-09 01:55:48 -0800

    syscall

--- syscall.c
+++ syscall.c
@@ -8,17 +8,20 @@
 
 #define ITERATIONS 10
 
-int main () {
+int main (int argc, const char * argv[]) {
     unsigned long long start;
     unsigned long long end;
     uint32_t low, low1;
     uint32_t high, high1;
     int i;
+    char cwd[1024];
 
+    int iterations = atoi((const char *) argv[argc-1]);
     WARMUP(high, low, high1, low1);
-    for (i = 0; i < ITERATIONS; i++) {
+
+    for (i = 0; i < iterations; i++) {
         START_COUNT(high, low);
-        getpid();
+        getcwd(cwd, sizeof(cwd)); 
         STOP_COUNT(high1, low1);
 
         start = ((unsigned long long) high << 32) | low;
