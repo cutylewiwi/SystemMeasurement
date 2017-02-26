d600367710109de88d4477498dc88aa005e4cc6b
cuty-lewiwi <wuyudong5426@163.com>
2017-02-25 23:55:08 -0800

    read bandwidth

---
+++ Memory/bandwidth.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include <time.h>
+#include <memory.h>
+
+#include "proj_timing.h"
+
+// TODO: random permutation stride
+
+#define	FIVE(m)		m m m m m
+#define	TEN(m)		FIVE(m) FIVE(m)
+#define	FIFTY(m)	TEN(m) TEN(m) TEN(m) TEN(m) TEN(m)
+#define	HUNDRED(m)	FIFTY(m) FIFTY(m)
+#define FIVEHUN(m)  HUNDRED(m) HUNDRED(m) HUNDRED(m) HUNDRED(m) HUNDRED(m)
+#define THOUSAND(m) FIVEHUN(m) FIVEHUN(m)
+#define ITERATIONS  3
+#define CHUNKS      32
+#define MEGA        (1 << 20)
+#define CACHE       (8 * MEGA)
+
+int main (int argc, const char * argv[]) {
+    int i;
+    unsigned int random_index[6];
+    unsigned char * large_read;
+    unsigned char * large_write;
+    unsigned char * l3cache;
+    uint32_t low, low1;
+    uint32_t high, high1;
+    unsigned long long start;
+    unsigned long long end;
+
+    srand((unsigned)time(NULL));
+
+    large_read = (unsigned char *) malloc(CHUNKS * CACHE);
+    large_write = (unsigned char *) malloc(CHUNKS * CACHE);
+    l3cache = (unsigned char *) malloc(CACHE);
+
+    for (i = 0; i < CHUNKS * CACHE / sizeof(unsigned char); i++) {
+        large_read[i] = (unsigned char) rand() % 255;
+    }
+
+    for (i = 0; i < 6; i++) {
+        random_index[i] = rand() % CHUNKS;
+    }
+
+    WARMUP(high, low, high1, low1);
+
+    for (i = 0; i <  ITERATIONS; i++) {
+        START_COUNT(high, low);
+#define INST memcpy(l3cache, &large_read[random_index[i] * CACHE], CACHE);
+        HUNDRED(INST);
+        STOP_COUNT(high1, low1);
+
+        start = ((unsigned long long) high << 32) | low;
+        end = ((unsigned long long) high1 << 32) | low1;
+
+        printf("read%d:\t %llu\n", (end - start) / 1000);
+    }
+
+    return 0;
+}
