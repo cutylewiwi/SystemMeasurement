a815eb8df0e90b4fb2465ae70c37287173e94f7f
cuty-lewiwi <wuyudong5426@163.com>
2017-03-20 01:54:58 -0700

    test

--- CPU/syscall_getcwd_int.c
+++ CPU/syscall_getcwd_int.c
@@ -28,24 +28,25 @@ int main (int argc, const char * argv[]) {
     uint32_t low, low1;
     uint32_t high, high1;
     int i;
-    pid_t pid;
+    char * ret;
     char cwd[1024];
 
     int iterations = atoi((const char *) argv[argc-1]);
     WARMUP(high, low, high1, low1);
-
+    getcwd(cwd, sizeof(cwd));
+    printf("%s\n", cwd);
     for (i = 0; i < iterations; i++) {
         START_COUNT(high, low);
 
         asm volatile
         (
             "int $0x80"
-            : "=a" (pid)
+            : "=a" (ret)
             : "0"(__NR_getcwd), "b"(cwd), "c"(sizeof(cwd))
             : "cc", "edi", "esi", "memory"
         );
         STOP_COUNT(high1, low1);
-
+        printf("%s\n", cwd);
         // printf("pid: %d\n", pid1);
 
         start = ((unsigned long long) high << 32) | low;
